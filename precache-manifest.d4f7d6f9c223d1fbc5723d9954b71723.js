afcf778487f3ef86bfd5dd87aded9cebdd2c41db
tratchapong <tratchapong@gmail.com>
2020-10-30 03:30:06 +0700

    Updates

---
+++ precache-manifest.d4f7d6f9c223d1fbc5723d9954b71723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e22f4b7652df2af0150b0e5b0783c38",
+    "url": "/WorldClock/index.html"
+  },
+  {
+    "revision": "5cad74a905f71e1b357d",
+    "url": "/WorldClock/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5cad74a905f71e1b357d",
+    "url": "/WorldClock/static/js/2.6f540914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WorldClock/static/js/2.6f540914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04948115593e7f86196b",
+    "url": "/WorldClock/static/js/main.6d710496.chunk.js"
+  },
+  {
+    "revision": "53d9ff83b98968b3dfcf",
+    "url": "/WorldClock/static/js/runtime-main.37f7eb61.js"
+  }
+]);
