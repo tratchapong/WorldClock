ed03579ad7acabe8edec28f3c6d29e48c7f85845
tratchapong <tratchapong@gmail.com>
2020-11-22 00:45:20 +0700

    refactor AppClock to array.map only

--- src/AppClock.js
+++ src/AppClock.js
@@ -5,26 +5,18 @@ import 'react-clock/dist/Clock.css';
 function AppClock() {
   const [options, setOptions] = useState([
      { time : new Date(), tz : 'Asia/Bangkok'}, 
-     { time : new Date(), tz : 'Australia/Brisbane'} 
+     { time : new Date(), tz : 'Australia/Brisbane'}, 
+     { time : new Date(), tz : 'America/New_York'}, 
+     { time : new Date(), tz : 'Europe/Istanbul'} 
     ])
-  const [value, setValue] = useState(new Date())
-  const [value2, setValue2] = useState(new Date())
-  const [timeZone, setTimeZone] = useState('Asia/Bangkok')
-  const [timeZone2, setTimeZone2] = useState('Australia/Brisbane')
 
   const updateClock = () => {
-    let tz =  new Date().toLocaleString("en-US", {timeZone : timeZone})
-    let tz2 =  new Date().toLocaleString("en-US", {timeZone : timeZone2})
-    setValue(new Date(tz))
-    setValue2(new Date(tz2))
     
       let newOpt = options.map( x => {
         let newtz = new Date().toLocaleString("en-US", {timeZone : x.tz})
         x.time = new Date(newtz)
-        // console.log(x.time)
         return ({time: x.time, tz: x.tz})
       })
-      // console.log('NEWOPT : ', newOpt)
       setOptions(newOpt)
 
   }
@@ -37,19 +29,17 @@ function AppClock() {
   }, [])
 
   return (
-    <div>
-      <p>Current Time of <span style={{background:"gold"}}>{timeZone}</span></p>
-      <Clock value={value} />
-      <hr />
-      <p>Current Time of <span style={{background:"gold"}}>{timeZone2}</span></p>
-      <Clock value={value2} />
-      <hr />
-
+    <div style={{width:"fit-content", margin:"0 auto", textAlign:"center"}}>
       {options.map( x => {
         console.log(x)
         return (
-          <div>
-            <p>Current Time of <span style={{background:"lime"}}> {x.tz}</span></p>
+          <div style={{color: "whitesmoke"}}>
+            {
+             x.time.getHours()<12 ?
+             <p><span style={{background:"tomato"}}> {x.tz}</span></p> 
+             : 
+             <p><span style={{background:"darkblue"}}> {x.tz}</span></p>
+             }
             <Clock value={x.time} />
             <hr />
           </div>
